431396dc4069f3ca90941f31221442b6f6d65d76
github.com:photo2story <photo2story@naver.com>
2024-08-12 16:24:45 +0900

    delta_Previous_Relative_Divergence

--- my-flask-app/gemini.py
+++ my-flask-app/gemini.py
@@ -135,7 +135,7 @@ async def analyze_with_gemini(ticker):
            리뷰주식의 누적수익률 = {final_rate}
            기준이 되는 비교주식(S&P 500, VOO)의 누적수익율 = {final_rate_vs}
            이격도 (max: {max_divergence}, min: {min_divergence}, 현재: {current_divergence}, 상대이격도: {relative_divergence})
-           최근 상대이격도 변화량 = {delta_Previous_Relative_Divergence} , (-): 단기하락, (+): 단기상승
+
         2) 제공된 자료의 최근 주가 변동(간단하게: 5일, 20일, 60일 이동평균 수치로):
            종가 = {Close}
            5일이동평균 = {sma_5}
@@ -144,6 +144,7 @@ async def analyze_with_gemini(ticker):
         3) 제공된 자료의 RSI, PPO 인덱스 지표를 분석해줘 (간단하게):
            RSI = {rsi}
            PPO = {ppo}
+           최근 상대이격도 변화량 = {delta_Previous_Relative_Divergence} , (-): 단기하락, (+): 단기상승
         4) 최근 실적 및 전망: 제공된 자료의 실적을 분석해줘(간단하게)
            실적 = {earnings_text} 표로 제공된 실적을 분석해줘
            가장 최근 실적은 예상치도 함께 포함해서 검토해줘
