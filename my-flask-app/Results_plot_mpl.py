08ec8e7efa43968d921e59b1f4dbf24d9209f6b9
github.com:photo2story <photo2story@naver.com>
2024-09-01 00:34:46 +0900

    Auto-commit moved files

--- my-flask-app/Results_plot_mpl.py
+++ my-flask-app/Results_plot_mpl.py
@@ -20,6 +20,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Noto Sans KR 폰트 파일 경로 (Regular 스타일 사용)
 font_path = os.path.join(current_dir, '..', 'Noto_Sans_KR', 'NotoSansKR-Regular.ttf')
+print("Font path:", font_path)
 
 # 폰트 속성 설정
 font_prop = fm.FontProperties(fname=font_path)
