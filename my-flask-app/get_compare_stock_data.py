15b08114d8af52a429a48d51fe362a2447f5f724
github.com:photo2story <photo2story@naver.com>
2024-09-01 02:46:32 +0900

    Auto-commit moved files

--- my-flask-app/get_compare_stock_data.py
+++ my-flask-app/get_compare_stock_data.py
@@ -82,9 +82,9 @@ def calculate_potential_profit_and_loss(df, current_relative_divergence):
         max_potential_loss = current_relative_divergence - min_divergence
 
         # Calculate time differences
-        max_divergence_date = df[df['Relative_Divergence'] == max_divergence].index[-1]
-        min_divergence_date = df[df['Relative_Divergence'] == min_divergence].index[-1]
-        current_date = df.index[-1]
+        max_divergence_date = pd.to_datetime(df[df['Relative_Divergence'] == max_divergence].iloc[-1]['Date'])
+        min_divergence_date = pd.to_datetime(df[df['Relative_Divergence'] == min_divergence].iloc[-1]['Date'])
+        current_date = pd.to_datetime(df.iloc[-1]['Date'])
 
         time_to_max = (max_divergence_date - current_date).days
         time_to_min = (min_divergence_date - current_date).days
@@ -93,7 +93,7 @@ def calculate_potential_profit_and_loss(df, current_relative_divergence):
     except Exception as e:
         print(f"Error calculating potential profit/loss: {e}")
         return None, None, None, None
-    
+
 
 async def collect_relative_divergence():
     tickers = [stock for sector, stocks in config.STOCKS.items() for stock in stocks]
