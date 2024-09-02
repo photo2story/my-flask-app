f0d8aa0e6aae9d9ca8cfbcf4c7fa67076e60afc1
github.com:photo2story <photo2story@naver.com>
2024-09-03 00:37:35 +0900

    hone

--- my-flask-app/backtest_send.py
+++ my-flask-app/backtest_send.py
@@ -1,4 +1,5 @@
 # backtest_send.py
+# backtest_send.py
 import requests
 import os, sys
 import pandas as pd
@@ -6,6 +7,7 @@ import matplotlib.pyplot as plt
 from discord.ext import commands
 import discord  # discord 모듈 추가
 import asyncio
+from datetime import datetime  # 날짜 처리 추가
 
 # 사용자 정의 모듈 임포트
 from Results_plot import plot_comparison_results
@@ -20,29 +22,30 @@ import config
 # 루트 디렉토리를 sys.path에 추가
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-option_strategy =config.option_strategy # 시뮬레이션 전략 설정
+option_strategy = config.option_strategy  # 시뮬레이션 전략 설정
 
-# backtest_and_send.py
+def get_voo_last_date(cache_file):
+    if os.path.exists(cache_file):
+        df = pd.read_csv(cache_file)
+        return df['Date'].max()
+    return None
+
 async def get_voo_data(option_strategy, ctx):
-    # config.py에서 VOO_CACHE_FILE과 is_cache_valid 함수를 사용합니다.
+    voo_last_date = get_voo_last_date(config.VOO_CACHE_FILE)
     
-    if config.is_cache_valid(config.VOO_CACHE_FILE, config.START_DATE):
+    if voo_last_date and config.is_cache_valid(config.VOO_CACHE_FILE, config.START_DATE):
         await ctx.send("Using cached VOO data.")
-        # 캐시된 데이터를 불러옵니다.
         cached_voo_data = pd.read_csv(config.VOO_CACHE_FILE)
+        return cached_voo_data, voo_last_date
     else:
         await ctx.send("Fetching new VOO data.")
-        # VOO 데이터를 새로 가져옵니다.
         stock_data2, _ = get_stock_data('VOO', config.START_DATE, config.END_DATE)
         result_df2 = My_strategy.my_strategy(stock_data2, option_strategy)
         result_df2.rename(columns={'rate': 'rate_vs'}, inplace=True)
         
         await ctx.send("Saving new VOO data to cache.")
-        # 새로 가져온 데이터를 캐시 파일로 저장합니다.
         result_df2.to_csv(config.VOO_CACHE_FILE, index=False)
-        cached_voo_data = result_df2
-    
-    return cached_voo_data
+        return result_df2, result_df2['Date'].max()
 
 async def backtest_and_send(ctx, stock, option_strategy, bot=None):
     if bot is None:
@@ -57,15 +60,18 @@ async def backtest_and_send(ctx, stock, option_strategy, bot=None):
         return
     
     try:
-        await ctx.send(f'Fetching data for {stock}.')
+        # VOO 데이터 가져오기 (캐시된 데이터 사용 또는 새로 가져오기)
+        result_df2, voo_last_date = await get_voo_data(option_strategy, ctx)
         
-        # 주식 데이터 가져오기
-        stock_data, _ = get_stock_data(stock, config.START_DATE, config.END_DATE)
+        # END_DATE 재설정
+        end_date = min(datetime.strptime(voo_last_date, '%Y-%m-%d').date(), config.END_DATE)
+        
+        await ctx.send(f'Fetching data for {stock} up to {end_date}.')
+        
+        # 주식 데이터 가져오기 (재설정된 END_DATE 사용)
+        stock_data, _ = get_stock_data(stock, config.START_DATE, end_date)
         await ctx.send(f'Running strategy for {stock}.')
         result_df = My_strategy.my_strategy(stock_data, option_strategy)
-        
-        # VOO 데이터 가져오기 (캐시된 데이터 사용 또는 새로 가져오기)
-        result_df2 = await get_voo_data(option_strategy, ctx)
         
         await ctx.send(f'Combining data for {stock} with VOO data.')
         
@@ -75,10 +81,6 @@ async def backtest_and_send(ctx, stock, option_strategy, bot=None):
         
         # 주식 데이터와 VOO 데이터 병합
         combined_df = result_df.merge(result_df2[['Date', 'rate_vs']], on='Date', how='left')
-        
-        # VOO 데이터의 마지막 날짜를 기준으로 데이터를 자릅니다.
-        voo_last_date = result_df2['Date'].max()
-        combined_df = combined_df[combined_df['Date'] <= voo_last_date]
 
         # 누락된 값을 0으로 채우기
         combined_df.fillna(0, inplace=True)
@@ -89,9 +91,22 @@ async def backtest_and_send(ctx, stock, option_strategy, bot=None):
         # 주요 거래 데이터가 모두 유효한 행만 유지
         combined_df = combined_df[combined_df[main_columns].apply(lambda row: all(row != 0) and all(row.notna()), axis=1)]
 
-        # 결과 CSV 파일로 저장하기
+        # 현재 작업 디렉토리 가져오기
+        current_directory = os.getcwd()
+
+        # 현재 디렉토리에 따라 저장 경로 설정
+        if 'my-flask-app' in current_directory:
+            # 테스트 환경의 경우
+            base_path = os.path.join('..', 'static', 'images')
+        else:
+            # 서버 구동 환경의 경우
+            base_path = os.path.join('static', 'images')
+
+        # 안전한 파일 이름 생성
         safe_ticker = stock.replace('/', '-')
-        file_path = os.path.join('static', 'images', f'result_VOO_{safe_ticker}.csv')
+        file_path = os.path.join(base_path, f'result_VOO_{safe_ticker}.csv')
+
+        # 파일 저장
         await ctx.send(f'Saving results to {file_path}.')
         combined_df.to_csv(file_path, float_format='%.2f', index=False)
         
@@ -144,6 +159,7 @@ async def test_backtest_and_send():
 if __name__ == "__main__":
     print("Starting test for back-testing.")
     asyncio.run(test_backtest_and_send())
+
 
     # python backtest_send.py        
 
