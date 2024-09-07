b293f992ba3f58bf13dff3f06137a1f1336c24e6
github.com:photo2story <photo2story@naver.com>
2024-09-08 08:09:55 +0900

    Auto-commit moved files

--- my-flask-app/Results_plot.py
+++ my-flask-app/Results_plot.py
@@ -160,7 +160,7 @@ async def plot_comparison_results(ticker, start_date, end_date):
 
 if __name__ == "__main__":
     print("Starting test for plotting results.")
-    ticker = "TSLA"
+    ticker = "AAPL"
     start_date = config.START_DATE
     end_date = config.END_DATE
     print(f"Plotting results for {ticker} from {start_date} to {end_date}")
