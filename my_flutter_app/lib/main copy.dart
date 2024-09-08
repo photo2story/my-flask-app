8c8ae1b91d03378613aac26efbaea6519c347dd6
github.com:photo2story <photo2story@naver.com>
2024-09-08 13:53:15 +0900

    flutter OK!!!

--- my_flutter_app/lib/main copy.dart
+++ my_flutter_app/lib/main copy.dart
@@ -1,316 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:photo_view/photo_view.dart';
 
-
-void main() async {
-  // API 키와 디버그 모드 초기화
-  Gemini.init(
-    apiKey: const String.fromEnvironment('apiKey'),
-    enableDebugging: true,
-  );
-
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stock Comparison Review',
+      title: 'Image Test',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: ImageTestPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _comparisonImageUrl = '';
-  String _resultImageUrl = '';
-  String _reportText = '';
-  String _message = '';
-  List<String> _tickers = [];
-  final List<Map<String, String>> _messages = [];
-  final TextEditingController _controller = TextEditingController();
-
-  final String apiUrl = 'https://api.github.com/repos/photo2story/my-flutter-app/contents/static/images';
-
-  @override
-  void initState() {
-    super.initState();
-    fetchReviewedTickers();
-  }
-
-  Future<void> fetchReviewedTickers() async {
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-      if (response.statusCode == 200) {
-        final List<dynamic> files = json.decode(response.body);
-        final tickers = files
-            .where((file) => file['name'].startsWith('comparison_') && file['name'].endsWith('_VOO.png'))
-            .map<String>((file) => file['name'].replaceAll('comparison_', '').replaceAll('_VOO.png', ''))
-            .toList();
-
-        setState(() {
-          _tickers = tickers;
-        });
-      } else {
-        setState(() {
-          _message = 'Error occurred while fetching tickers: ${response.statusCode}';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _message = 'Error occurred while fetching tickers: $e';
-      });
-    }
-  }
-
-  Future<void> fetchImagesAndReport(String stockTicker) async {
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-      if (response.statusCode == 200) {
-        final List<dynamic> files = json.decode(response.body);
-        final comparisonFile = files.firstWhere(
-            (file) => file['name'] == 'comparison_${stockTicker}_VOO.png',
-            orElse: () => null);
-        final resultFile = files.firstWhere(
-            (file) => file['name'] == 'result_mpl_${stockTicker}.png',
-            orElse: () => null);
-        final reportFile = files.firstWhere(
-            (file) => file['name'] == 'report_${stockTicker}.txt',
-            orElse: () => null);
-
-        if (comparisonFile != null && resultFile != null) {
-          setState(() {
-            _comparisonImageUrl = comparisonFile['download_url'];
-            _resultImageUrl = resultFile['download_url'];
-            _message = '';
-          });
-          if (reportFile != null) {
-            final reportResponse = await http.get(Uri.parse(reportFile['download_url']));
-            if (reportResponse.statusCode == 200) {
-              setState(() {
-                _reportText = reportResponse.body;
-                _messages.add({'bot': 'Report Loaded: \n' + _reportText});
-              });
-              // ReportView를 모달로 보여줍니다 (기존 화면 유지)
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => Dialog(
-                  child: ReportView(reportText: _reportText),
-                ),
-              );
-            } else {
-              setState(() {
-                _reportText = 'Failed to load report text';
-                _messages.add({'bot': _reportText});
-              });
-            }
-          } else {
-            setState(() {
-              _reportText = ''; // Clear the report text if no report is found
-            });
-          }
-        } else {
-          setState(() {
-            _comparisonImageUrl = '';
-            _resultImageUrl = '';
-            _reportText = ''; // Clear the report text if no images or report are found
-            _message = 'Unable to find images or report for the stock ticker $stockTicker';
-            _messages.add({'bot': _message});
-          });
-        }
-      } else {
-        setState(() {
-          _comparisonImageUrl = '';
-          _resultImageUrl = '';
-          _reportText = ''; // Clear the report text on API call failure
-          _message = 'GitHub API call failed: ${response.statusCode}';
-          _messages.add({'bot': _message});
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _comparisonImageUrl = '';
-        _resultImageUrl = '';
-        _reportText = ''; // Clear the report text on exception
-        _message = 'Error occurred: $e';
-        _messages.add({'bot': _message});
-      });
-    }
-  }
-
-  Future<void> _sendMessageToGemini(String message) async {
-    try {
-      final response = await Gemini.sendMessage(
-        prompt: message,
-        model: 'gemini-1.5-flash',  // 사용하는 모델 지정
-      );
-
-      if (response.statusCode == 200) {
-        final reply = response.data['choices'][0]['text'];
-        setState(() {
-          _messages.add({'bot': reply});
-        });
-      } else {
-        setState(() {
-          _messages.add({'bot': 'Error: Unable to communicate with Gemini API.'});
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _messages.add({'bot': 'Error: $e'});
-      });
-    }
-  }
-
-  void _handleSubmitted(String text) {
-    setState(() {
-      _messages.add({'user': text});
-    });
-
-    // 주식 티커를 기반으로 한 요청인지, 일반 질문인지 구분
-    if (_tickers.contains(text.toUpperCase())) {
-      fetchImagesAndReport(text.toUpperCase());
-    } else {
-      _sendMessageToGemini(text);
-    }
-
-    _controller.clear();
-  }
+class ImageTestPage extends StatelessWidget {
+  final String imageUrl = 'http://192.168.0.5:5000/static/images/comparison_AAPL_VOO.png';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Stock Comparison Review'),
+        title: Text('Image Test'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                final message = _messages[index];
-                return ListTile(
-                  title: Text(message.keys.first == 'user' ? 'You: ' : 'Bot: '),
-                  subtitle: Text(message.values.first),
-                );
-              },
-            ),
-            if (_comparisonImageUrl.isNotEmpty)
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                height: 400, // 고정된 높이 설정
-                child: Image.network(
-                  _comparisonImageUrl,
-                  fit: BoxFit.contain,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Text('Failed to load comparison image');
-                  },
-                ),
-              ),
-            if (_resultImageUrl.isNotEmpty)
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                height: 400, // 고정된 높이 설정
-                child: Image.network(
-                  _resultImageUrl,
-                  fit: BoxFit.contain,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Text('Failed to load result image');
-                  },
-                ),
-              ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _controller,
-                      onSubmitted: _handleSubmitted,
-                      decoration: InputDecoration(
-                        labelText: 'Ask Gemini or Enter Stock Ticker...',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.send),
-                    onPressed: () => _handleSubmitted(_controller.text),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ReportView extends StatelessWidget {
-  final String reportText;
-
-  ReportView({required this.reportText});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Stock Report'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Markdown(
-          data: reportText,
-          styleSheet: MarkdownStyleSheet(
-            h1: TextStyle(color: Colors.blue, fontSize: 24, fontWeight: FontWeight.bold),
-            h2: TextStyle(color: Colors.blueAccent, fontSize: 22, fontWeight: FontWeight.bold),
-            p: TextStyle(fontSize: 18, height: 1.5),
-            strong: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            blockSpacing: 8.0,
-            listBullet: TextStyle(color: Colors.blueAccent),
-            tableHead: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-              color: Colors.white,
-              backgroundColor: Colors.blueAccent,
-            ),
-            tableBody: TextStyle(fontSize: 18, color: Colors.black),
+      body: Center(
+        child: GestureDetector(
+          onTap: () => _openImage(context),
+          child: Image.network(
+            imageUrl,
+            errorBuilder: (context, error, stackTrace) {
+              return Text('Failed to load image: $error');
+            },
           ),
-          selectable: true, // 선택 가능한 텍스트를 위해 추가
         ),
+      ),
+    );
+  }
+
+  void _openImage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ImagePreviewScreen(imageUrl: imageUrl),
       ),
     );
   }
 }
 
+class ImagePreviewScreen extends StatelessWidget {
+  final String imageUrl;
 
-// flutter clean
+  ImagePreviewScreen({required this.imageUrl});
 
-// flutter pub add flutter_gemini
-// flutter pub get
-
-// flutter devices
-
-// flutter run -d R3CX404VPHE
-
-// flutter run -d chrome
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preview Image'),
+      ),
+      body: Center(
+        child: PhotoView(
+          imageProvider: NetworkImage(imageUrl),
+          errorBuilder: (context, error, stackTrace) {
+            return Text('Failed to load image: $error');
+          },
+        ),
+      ),
+    );
+  }
+}
--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,67 +11,232 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Test',
+      title: 'Stock Comparison Review',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ImageTestPage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class ImageTestPage extends StatelessWidget {
-  final String imageUrl = 'http://192.168.0.5:5000/static/images/comparison_AAPL_VOO.png';
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _comparisonImageUrl = '';
+  String _resultImageUrl = '';
+  String _reportText = '';
+  List<String> _tickers = [];
+  bool _isLoading = false;
+  String _selectedTicker = "AAPL"; // 기본 값으로 Apple 주식 티커 설정
+  final TextEditingController _controller = TextEditingController();
+
+  final String apiUrl = 'https://api.github.com/repos/photo2story/my-flutter-app/contents/static/images';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchReviewedTickers();
+    fetchImagesAndReport(_selectedTicker); // 앱이 시작될 때 AAPL 주식 데이터를 불러옴
+  }
+
+  Future<void> fetchReviewedTickers() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final List<dynamic> files = json.decode(response.body);
+        final tickers = files
+            .where((file) => file['name'].startsWith('comparison_') && file['name'].endsWith('_VOO.png'))
+            .map<String>((file) => file['name'].replaceAll('comparison_', '').replaceAll('_VOO.png', ''))
+            .toList();
+
+        setState(() {
+          _tickers = tickers;
+        });
+      } else {
+        setState(() {
+          _comparisonImageUrl = '';
+          _resultImageUrl = '';
+          _reportText = 'Error occurred while fetching tickers: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _comparisonImageUrl = '';
+        _resultImageUrl = '';
+        _reportText = 'Error occurred while fetching tickers: $e';
+      });
+    }
+  }
+
+  Future<void> fetchImagesAndReport(String stockTicker) async {
+    setState(() {
+      _isLoading = true;
+      _comparisonImageUrl = 'https://raw.githubusercontent.com/photo2story/my-flutter-app/main/static/images/comparison_${stockTicker}_VOO.png';
+      _resultImageUrl = '';
+      _reportText = '';
+    });
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final List<dynamic> files = json.decode(response.body);
+        final resultFile = files.firstWhere(
+            (file) => file['name'] == 'result_mpl_${stockTicker}.png',
+            orElse: () => null);
+        final reportFile = files.firstWhere(
+            (file) => file['name'] == 'report_${stockTicker}.txt',
+            orElse: () => null);
+
+        if (resultFile != null) {
+          setState(() {
+            _resultImageUrl = resultFile['download_url'];
+          });
+        }
+
+        if (reportFile != null) {
+          final reportResponse = await http.get(Uri.parse(reportFile['download_url']));
+          if (reportResponse.statusCode == 200) {
+            setState(() {
+              _reportText = reportResponse.body;
+            });
+          } else {
+            setState(() {
+              _reportText = 'Failed to load report text';
+            });
+          }
+        } else {
+          setState(() {
+            _reportText = ''; // Clear the report text if no report is found
+          });
+        }
+      } else {
+        setState(() {
+          _resultImageUrl = '';
+          _reportText = 'GitHub API call failed: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _resultImageUrl = '';
+        _reportText = 'Error occurred: $e';
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Image Test'),
+        title: Text('Stock Comparison Review'),
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () => _openImage(context),
-          child: Image.network(
-            imageUrl,
-            errorBuilder: (context, error, stackTrace) {
-              return Text('Failed to load image: $error');
-            },
-          ),
-        ),
-      ),
-    );
-  }
+      body: InteractiveViewer( // 전체 화면에 적용
+        boundaryMargin: EdgeInsets.all(8),
+        minScale: 0.5,
+        maxScale: 3.0,
+        child: Row(
+          children: <Widget>[
+            // 왼쪽 패널: 티커 리스트
+            Container(
+              width: MediaQuery.of(context).size.width * 0.15,
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.grey[200],
+              child: ListView(
+                children: _tickers.map((ticker) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 2.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedTicker = ticker;
+                        });
+                        fetchImagesAndReport(ticker);
+                      },
+                      child: Text(
+                        ticker,
+                        style: TextStyle(fontSize: 11, color: Colors.blue, height: 1.2),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
 
-  void _openImage(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => ImagePreviewScreen(imageUrl: imageUrl),
+            // 오른쪽 패널: 상단 - 고정 이미지, 하단 - 결과 이미지 및 리포트
+            Expanded(
+              child: Column(
+                children: [
+                  // 로딩 중 표시
+                  if (_isLoading)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircularProgressIndicator(),
+                    ),
+
+                  // 고정된 comparison_${stockTicker}_VOO.png 이미지
+                  _comparisonImageUrl.isNotEmpty
+                      ? Container(
+                          padding: const EdgeInsets.all(8.0),
+                          height: 230, // 고정 크기 설정
+                          child: Image.network(
+                            _comparisonImageUrl,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Text('Failed to load comparison image');
+                            },
+                          ),
+                        )
+                      : Container(),
+
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          // 스크롤 가능한 result_mpl_${stockTicker}.png 이미지
+                          _resultImageUrl.isNotEmpty
+                              ? Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Image.network(
+                                    _resultImageUrl,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return Text('Failed to load result image');
+                                    },
+                                  ),
+                                )
+                              : Container(),
+
+                          // 리포트 텍스트
+                          _reportText.isNotEmpty
+                              ? Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: MarkdownBody(
+                                    data: _reportText,
+                                  ),
+                                )
+                              : Container(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class ImagePreviewScreen extends StatelessWidget {
-  final String imageUrl;
 
-  ImagePreviewScreen({required this.imageUrl});
+// flutter devices
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Preview Image'),
-      ),
-      body: Center(
-        child: PhotoView(
-          imageProvider: NetworkImage(imageUrl),
-          errorBuilder: (context, error, stackTrace) {
-            return Text('Failed to load image: $error');
-          },
-        ),
-      ),
-    );
-  }
-}
+// flutter run -d R3CX404VPHE
+
+// flutter run -d chrome
